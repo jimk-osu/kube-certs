78c6344f56e755f5160b9442485cf64af3753d7d
stormlab01 <jim.kittle@stormlabs.io>
2018-02-02 16:58:07 -0500

    sucking real good on this new fangled GO

--- kube-certs.go
+++ kube-certs.go
@@ -10,28 +10,64 @@
 */
 package main
 
-import ("github.com/cloudflare/cfssl/cmd/cfssl/cfssl"
+import (
+  //"github.com/cloudflare/cfssl/cmd/cfssl/cfssl"
   "fmt"
   "encoding/json"
-  "errors"
+  //"errors"
   "io/ioutil"
   "os")
 
+// type Worker struct {
+//   node Kubenode[]
+// }
+//
+// type Controller struct {
+//   node Kubenode[]
+// }
 
+type KubecertConfg struct{
+ Nodes struct {
+  Workers []struct {
+    Hostname string'json:"hostname"'
+    ExternalIP string 'json:"externalIP"'
+    InternalIP string 'json:"internalIP"'
+  } `json:"workers"`
+  Controllers []struct {
+    Hostname string 'json:"hostname"'
+    ExternalIP string 'json:"externalIP"'
+    InternalIP string 'json:"internalIP"'
+  } `json:"controllers"`
+ } 'json:"nodes"'
+ CA struct {
+   location string 'json:"location"'
+   CN string 'json:"CN"'
+   ORG string 'json:"ORG"'
+   OU string 'json:"OU"'
+   ST string 'json:"ST"'
+ } 'json:"ca"'
+}
 
 func main(){
-  fmt.Println()
+  readKubeCertJson()
 }
 
 func readKubeCertJson(){
-  raw, err := ioutil.ReadFile("./kube-cert.json")
+  file, err := ioutil.ReadFile("./kube-cert-config.json")
 
   if err != nil {
         fmt.Println(err.Error())
         os.Exit(1)
     }
 
-    var c []Page
-    json.Unmarshal(raw, &c)
-    return c
+    fmt.Printf("%s\n", string(file))
+
+    //var c []Page
+    //var jsontype Kubecertconfig
+    //json.Unmarshal(file, &jsontype)
+    // fmt.Printf("Results: %v\n", jsontype)
+    var f interface{}
+    err := json.Unmarshal(file, $f)
+
+    fmt.Printf("Results: %v\n", f)
 }
